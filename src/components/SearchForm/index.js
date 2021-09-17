e199c06cdf7b1b3756de03b856e21d4b882550f8
Martin Thurin Prats <martinthurinprats@MacBook-Pro-de-Martin.local>
2021-09-17 23:33:51 +0200

    commit_dispatch

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,33 +1,47 @@
-import React, { useReducer, useState } from 'react'
+import React, { useReducer } from 'react'
 import { useLocation } from "wouter"
 import css from "./SearchForm.module.css"
 
 const RATING = ['g', 'pg', 'pg-13', 'r']
 
-const reducer = (state, param) => {
-    return {
-        ...state,
-        keyword: param,
-        times: state.times + 1
+const ACTIONS = {
+    UPDATE_KEYWORD: 'update_keyword',
+    UPDATE_RATING: 'update_rating'
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case ACTIONS.UPDATE_KEYWORD:
+            return {
+                ...state,
+                keyword: action.payload,
+                times: state.times + 1
+            } 
+        
+        case ACTIONS.UPDATE_RATING:
+            return {
+                ...state,
+                rating: action.payload
+            }
+    
+        default:
+            return state
     }
 }
 
 
 export default function SearchForm({ initialKeyword = '', initialRating = 'g'}) {
-    const [rating, setRating] = useState(initialRating)
+    //const [rating, setRating] = useState(initialRating)
 
     const [state, dispatch] = useReducer(reducer, {
         keyword: decodeURIComponent(initialKeyword),
+        rating: initialRating,
         times: 0
     })
 
-    const {keyword, times} = state
+    const {keyword, rating, times} = state
 
     const [, pushLocation] = useLocation()
-
-    const updateKeyword = (keyword) => {
-        dispatch(keyword)
-    }
 
 
     const handleSubmit = (event) => {
@@ -37,11 +51,11 @@ export default function SearchForm({ initialKeyword = '', initialRating = 'g'}) 
     }
 
     const handleChange = (event) => {
-        updateKeyword(event.target.value)
+        dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: event.target.value })
     }
 
     const handleChangeRating = (event) => {
-        setRating(event.target.value)
+        dispatch({type: ACTIONS.UPDATE_RATING, payload: event.target.value })
     }
 
     return (
